8fa6488562bb553c243c8c7c309238f8021cb3a6
flyingbird95 <patrickvogel@live.nl>
2018-04-15 17:04:51 +0200

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # RepoFetcher
-A program to download a list of repositories from this organisation and read the docs-folder
+A program for downloading a list of repositories from this organisation and read the docs-folder.
+
+#### Requirements
+* NodeJS - For running the program
+* NPM - For installing the GitHub API library
+* SVN - For downloading a folder from GitHub through the command line.
+
+#### Running the program
+`npm start` - This will install the GitHub API library and then run the program.
+
+Alternatively, you can run `npm install` followed by `node index.js`.
+
+#### Notes
+Since the GitHub API is quite limited for unauthenticated users, this program can only be ran a couple of times before GitHub blocks your IP address (for a limited time) for exceeding the API request limit.
---
+++ index.js
@@ -0,0 +1,48 @@
+const GitHub = require("github-api");
+const { exec } = require("child_process");
+
+/* The organization name to fetch the repo's from */
+const username = "RUGSoftEng";
+/* What folder to fetch from the repo */
+const folder = "docs";
+/* Where we should store the downloaded content */
+const destination = "./docs";
+/* Prefix to use as a filter for the repo name */
+const repoPrefix = "2018-";
+/* What branch to fetch from */
+const branch = "master";
+
+
+const gh = new GitHub();
+const org = gh.getOrganization(username);
+
+org.getRepos(function (err, repos) {
+	if (err) {
+		console.log(err.response.data.message + "\n\n");
+		throw err;
+	}
+
+	repos = repos.filter(repo => repo.name.substring(0, 5) === repoPrefix);
+	console.log(`Found ${repos.length} repo's.`);
+	//repos = [repos[repos.length - 1]];
+	repos.forEach(function (repo) {
+		const name = repo.name;
+		console.log(name);
+		repo = gh.getRepo(username, repo.name);
+
+		repo.getContents(branch, null, false, function (err, content) {
+			content = content.filter(c => c.name === folder);
+			if (content.length > 0) {
+				const cmd = `svn export --force https://github.com/${username}/${name}/trunk/${folder} ${destination}`;
+				exec(cmd, function (error, stdout, stderr) {
+					if (error || stderr)
+						console.log(`An error occurred running:\n${cmd}\nOutput:\n${error}\n${stderr}`);
+					else
+						console.log(`Downloading ${name}:\n${stdout}`);
+				});
+			} else
+				console.log(`Error: ${name} does not have a ${folder} folder.`);
+		});
+
+	});
+});
